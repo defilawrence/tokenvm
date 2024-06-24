1c456fbe5030ffaaa688e91c433ac7b07a6614c1
defilawrence <defilawrence@gmail.com>
2024-06-24 17:12:33 +0000

    Defichain

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Defichain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Deficoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "Defi"
 )
 
 var ID ids.ID
